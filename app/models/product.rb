2935977575c2bf4c13e37a3d71e0a9cc7158285a
Minh Duc <duc11t3bk@gmail.com>
2017-08-01 00:45:13 +0700

    task_api_product_index_search_show

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,15 @@ class Product < ApplicationRecord
 
   validates :name, :description, presence: true, length: {maximum: 255}
   validates :price, :cost_time, :image, presence: true
+
+  scope :search_by, -> params do
+    params[:status] ||= 1
+    params[:price_cond] ||= Settings.ransack.lteq
+    params[:cost_time_cond] ||= Settings.ransack.lteq
+    condition = {name_cont: params[:name], description_cont: params[:description],
+      status: params[:status]}
+    condition.merge! "price_#{params[:price_cond]}": params[:price] if params[:price]
+    condition.merge! "cost_time_#{params[:cost_time_cond]}": params[:cost_time] if params[:cost_time]
+    ransack(condition).result
+  end
 end
